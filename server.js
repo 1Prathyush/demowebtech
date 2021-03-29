f8c3874656fabd1bdf3044e49c3bdd36101feb05
1Prathyush <prathyushunni3@gmail.com>
2021-03-29 13:40:33 +0530

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
 
 app.use(express.static("public"));
 app.set("view engine","ejs");
